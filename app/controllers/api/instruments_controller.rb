7c7b4a304267bc674c4d4e82bec6fa70c411ca67
JingruZhang <zhang.jingru.93@gmail.com>
2018-04-19 16:39:02 -0700

    add show action for production and instrument

--- app/controllers/api/instruments_controller.rb
+++ app/controllers/api/instruments_controller.rb
@@ -3,4 +3,9 @@ class Api::InstrumentsController < ApplicationController
 	def index
 		render json: Instrument.all
 	end
+
+	def show
+	  instrument = Instrument.find(params[:id])
+	  render json: instrument
+	end
 end
--- app/controllers/api/productions_controller.rb
+++ app/controllers/api/productions_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductionsController < ApplicationController
 	def index
 		render json: Production.all
 	end
+
+	def show
+		production = Production.find(params[:id])
+	 	render json: production
+	end
 end
