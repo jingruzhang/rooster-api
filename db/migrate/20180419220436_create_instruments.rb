a093a651663dbcac4505dc9d56409645f4b23a0a
JingruZhang <zhang.jingru.93@gmail.com>
2018-04-19 16:02:41 -0700

    add seed data

--- db/migrate/20180419220436_create_instruments.rb
+++ db/migrate/20180419220436_create_instruments.rb
@@ -1,10 +1,10 @@
 class CreateInstruments < ActiveRecord::Migration[5.2]
   def change
     create_table :instruments do |t|
-      t.string :type
+      t.string :position
       t.integer :calls
       t.boolean :doubling, :default => false
-      t.boolean :principle :default => false
+      t.boolean :principle, :default => false
       t.boolean :concertmaster_principle_doubling, :default => false
       t.integer :fee
       t.timestamps
